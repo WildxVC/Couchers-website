f994e10d5d6f8ba90ac512b47823c90ad5397a52
WildxVC <vinayak.chhatrashali@gmail.com>
2024-01-09 15:36:14 +0530

    FInal Gsap animations

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,10 @@
 var tl = gsap.timeline()
 
-tl.from("nav",{
+tl.from("nav", {
     opacity: 0,
-    duration:1
-})
-
-tl.from(".nav-main,#logo-letter,.nav-links,.nav-actions", {
-    y: -100,
-    opacity: 0,
-    duration: 1,
-    stagger:0.5
-})
-
-tl.from(".right", {
-    // x:100,
-    opacity: 0,
-    x: 100,
     duration: 1
-
 })
+
 tl.from(".hero-texts,.hero-texts h2,.hero-texts h2 span", {
     x: -100,
     opacity: 0,
@@ -29,4 +15,26 @@ tl.from(".hero-actions", {
     y: 100,
     opacity: 0,
     // stagger:1
+})
+
+tl.from(".right", {
+    // x:100,
+    opacity: 0,
+    // x: 100,
+    duration: 1
+
+})
+
+
+gsap.from(".main-container2", {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.5,
+    // stagger:1,
+    scrollTrigger: {
+        trigger: ".main-container2",
+        scroller: "body",
+        // markers:true,
+        start: "top 70%"
+    }
 })
